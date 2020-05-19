9a3d2c712b132f9f03678fc906aad8e4edf13159
Alexey <avlad88@yandex.ru>
2020-05-20 01:56:01 +0500

    51.Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,21 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/ma.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9RsMwS_m8aQOw4zsiT8r90KmdU2cMDT8o0QsMAZvzbyfGedcl&usqp=CAU',
-                followed: false,
-                fullName: 'Aleksey',
-                status: 'I am a boss',
-                location: { city: 'Ekaterinburg', country: 'Russia' }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9RsMwS_m8aQOw4zsiT8r90KmdU2cMDT8o0QsMAZvzbyfGedcl&usqp=CAU',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am a boss',
-                location: { city: 'Mosko2', country: 'Russia' }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9RsMwS_m8aQOw4zsiT8r90KmdU2cMDT8o0QsMAZvzbyfGedcl&usqp=CAU',
-                followed: false,
-                fullName: 'Adrey',
-                status: 'I am a boss',
-                location: { city: 'Egorhino', country: 'Russia' }
-            }
-        ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.usersPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto} alt='' />
                     </div>
                     <div>
                         {u.followed
@@ -52,12 +31,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
